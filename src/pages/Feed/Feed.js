cbaf66ddd78ee107b73e8c39ddcf27244d2545e3
Andres Nodas <nodasiniesta@gmail.com>
2021-01-17 23:24:28 +0700

    Identifying Realtime Potential

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -23,7 +23,11 @@ class Feed extends Component {
     };
 
     componentDidMount() {
-        fetch('URL')
+        fetch('http://localhost:8080/auth/status', {
+            headers: {
+                Authorization: 'Bearer ' + this.props.token,
+            },
+        })
             .then((res) => {
                 if (res.status !== 200) {
                     throw new Error('Failed to fetch user status.');
@@ -39,6 +43,22 @@ class Feed extends Component {
 
         openSocket('http://localhost:8080');
     }
+
+    addPost = (post) => {
+        this.setState((prevState) => {
+            const updatedPosts = [...prevState.posts];
+            if (prevState.postPage === 1) {
+                if (prevState.posts.length >= 2) {
+                    updatedPosts.pop();
+                }
+                updatedPosts.unshift(post);
+            }
+            return {
+                posts: updatedPosts,
+                totalPosts: prevState.totalPosts + 1,
+            };
+        });
+    };
 
     loadPosts = (direction) => {
         if (direction) {
@@ -81,7 +101,16 @@ class Feed extends Component {
 
     statusUpdateHandler = (event) => {
         event.preventDefault();
-        fetch('URL')
+        fetch('http://localhost:8080/auth/status', {
+            method: 'PATCH',
+            headers: {
+                Authorization: 'Bearer ' + this.props.token,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                status: this.state.status,
+            }),
+        })
             .then((res) => {
                 if (res.status !== 200 && res.status !== 201) {
                     throw new Error("Can't update status!");
